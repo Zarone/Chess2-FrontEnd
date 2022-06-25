85702ef9bda57e793e3eb19a03633a0803305d8b
Zarone <redninjadolphin1@hotmail.com>
2022-06-25 18:28:01 -0400

    made buttons do stuff

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
+import {serverID} from "./helper-js/utils.js";
 import {getQuerystring} from "./helper-js/utils.js"
 
-window.onload = () => {
+window.onload = async () => {
     let roomID = getQuerystring().roomID
-    let roomID_Dom = document.getElementById("roomID")
     console.log("RoomID", roomID)
-    roomID_Dom.innerText = "Room ID: " + roomID
+    if (roomID == undefined){
+        let openRoomRaw = await fetch(serverID+"/getOpenRoom")
+        let openRoomJson = await openRoomRaw.json()
+        let openRoom = openRoomJson.roomID
+        console.log(openRoom)
+        window.location.href="../client/game.html?roomID="+openRoom.toString();
+    } else {
+        let roomID_Dom = document.getElementById("roomID")
+        roomID_Dom.innerText = "Room ID: " + roomID
+    }
 }
