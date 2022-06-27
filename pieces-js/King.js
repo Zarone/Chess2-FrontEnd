48dc59833102db4e44797294d75b27dd758cb583
Zarone <redninjadolphin1@hotmail.com>
2022-06-27 16:34:52 -0400

    king

--- pieces-js/King.js
+++ pieces-js/King.js
@@ -1,4 +1,6 @@
 import {Piece} from "./Piece.js"
+import { getVerticalAndHorizontal, verticalAndHorizontalToID } from "../helper-js/utils.js";
+import { notSameType } from "../helper-js/conditions.js";
 
 export class King extends Piece {
 
@@ -22,5 +24,81 @@ export class King extends Piece {
                 return "Black King.png"
             }
         }
+    }
+
+    getMoves(){
+        let {vertical, horizontal} = getVerticalAndHorizontal(this.position)
+            
+        let output = []
+        if ((vertical + 1) < 9){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical+1, horizontal), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+        if ((horizontal + 1) < 9){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical, horizontal+1), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+        if ((vertical - 1) > 0){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical-1, horizontal), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+        if ((horizontal - 1) > 0){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical, horizontal-1), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+
+
+        if ((vertical + 1) < 9 && (horizontal + 1) < 9){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical+1, horizontal+1), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+
+        if ((vertical - 1) > 0 && (horizontal - 1) > 0){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical-1, horizontal-1), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+
+        if ((vertical + 1) < 9 && (horizontal - 1) > 0){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical+1, horizontal-1), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+
+        if ((vertical - 1) > 0 && (horizontal + 1) < 9){
+            output.push(
+                {
+                    pos: verticalAndHorizontalToID(vertical-1, horizontal+1), 
+                    conditions: [notSameType]
+                }
+            )
+        }
+        return output
     }
 }
