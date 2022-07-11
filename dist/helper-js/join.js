0a0aba66c1d35349e3e8fca369678bfb3f1fb615
KernelDeimos <eric@centos1.rack1.local>
2022-07-10 23:15:18 -0400

    Add GameConfig

--- dist/helper-js/join.js
+++ dist/helper-js/join.js
@@ -18,9 +18,9 @@ window.onload = async () => {
     raw_join_timed_button.addEventListener("click", () => rawJoin(15))
 
     
-    room_join_button.addEventListener("click", ()=>{
-        window.location.href="../game.html?friendRoom=true&roomID="+room_join_id.value+"&timeLimit="+(room_join_time.value||"100");
-    })
+    // room_join_button.addEventListener("click", ()=>{
+    //     window.location.href="../game.html?friendRoom=true&roomID="+room_join_id.value+"&timeLimit="+(room_join_time.value||"100");
+    // })
 
 
     let roomsCount_Dom = document.getElementById("rooms-count")
@@ -29,3 +29,123 @@ window.onload = async () => {
 
     roomsCount_Dom.innerText = "Rooms: " + roomsCountJson.roomCount;
 }
+
+foam.ENUM({
+    package: 'chess2',
+    name: 'GameMode',
+
+    values: [
+        { name: 'PLAYER_VS_PLAYER'   , label: 'Two Players'   },
+
+        // TODO: implement these game modes
+        // { name: 'PLAYER_ALONE'       , label: 'Singleplayer'  },
+        // { name: 'PLAYER_VS_COMPUTER' , label: 'Battle the AI' },
+    ]
+});
+
+foam.CLASS({
+    package: 'chess2',
+    name: 'GameConfig',
+    
+    properties: [
+        {
+            name: 'mode',
+            class: 'Enum',
+            of: 'chess2.GameMode'
+        },
+        {
+            name: 'roomId',
+            class: 'String',
+            required: true
+        },
+        {
+            name: 'playerTimeLimit',
+            class: 'Duration',
+            label: 'Time per Player'
+        }
+    ],
+
+    actions: [
+        {
+            name: 'start',
+            buttonStyle: 'PRIMARY',
+            isEnabled: function ( roomId ) {
+                return roomId != '';
+            },
+            code: function () {
+                let url = "../game.html?friendRoom=true";
+                url += `&roomID=${this.roomId}`;
+                url += `&timeLimit=${this.playerTimeLimit || 100}`;
+                window.location.href = url;
+            }
+        }
+    ]
+});
+
+foam.CLASS({
+    package: 'chess2',
+    name: 'GameConfigView',
+    extends: 'foam.u2.View',
+
+    requires: [
+        'foam.u2.borders.Block'
+    ],
+
+    properties: [
+        {
+            name: 'data',
+            factory: function () {
+                return chess2.GameConfig.create();
+            }
+        }
+    ],
+
+    css: `
+        ^ {
+            margin-top: 40px;
+        }
+        ^ input, ^ select {
+            font-size: 1rem;
+        }
+        ^ button.btn {
+            width: 100%;
+        }
+        ^row {
+            display: flex;
+            gap: 1.5rem;
+        }
+        ^row .foam-u2-PropertyBorder-propHolder > * {
+            gap: 0 !important;
+        }
+        ^ .foam-u2-PropertyBorder-errorText {
+            font-size: 1rem;
+            color: #FAA;
+        }
+    `,
+    
+    methods: [
+        function render() {
+            this
+                .addClass()
+                .start('h2').add('Custom Game').end()
+                .start()
+                    .start()
+                        .addClass(this.myClass('row'))
+                        .tag(this.data.MODE.__)
+                        .tag(this.data.ROOM_ID.__)
+                        .tag(this.data.PLAYER_TIME_LIMIT.__)
+                    .end()
+                    .start('button')
+                        .addClasses(['btn', 'btn-primary', 'btn-block'])
+                        .attrs({
+                            disabled: this.data.roomId$.map(v => !v)
+                        })
+                        .add(this.data.START.label)
+                        .on('click', () => {
+                            this.data.start();
+                        })
+                    .end()
+                .end()
+        }
+    ]
+});
