94a2f293d718df5a53789bca3c0c5c46d6d8d196
Zarone <redninjadolphin1@hotmail.com>
2022-08-20 16:27:05 -0400

    new style stuff

--- src/components/settingsMenu.jsx
+++ src/components/settingsMenu.jsx
@@ -9,7 +9,7 @@ export default function SettingsMenu({customStyle, setCustomStyle, soundOn, setS
             <p className="h5">Choose your style</p>
             <button type="button" className="btn btn-primary btn-block m-2" disabled={customStyle==styleList.oat} onClick={()=>{setCustomStyle(styleList.oat)}}>Oats Style</button>
             <button type="button" className="btn btn-primary btn-block m-2" disabled={customStyle==styleList.pixel} onClick={()=>{setCustomStyle(styleList.pixel)}}>Pixel Style</button>
-            {/* <button type="button" className="btn btn-primary btn-block m-2" disabled={customStyle==styleList.highRes} onClick={()=>{setCustomStyle(styleList.highRes)}}>High Res Style</button> */}
+            <button type="button" className="btn btn-primary btn-block m-2" disabled={customStyle==styleList.highRes} onClick={()=>{setCustomStyle(styleList.highRes)}}>High Res Style</button>
         </div>
         <div className="container m-3">
             <p className="h5">Sound Effects</p>
--- src/helper-js/StyleManager.ts
+++ src/helper-js/StyleManager.ts
@@ -1,6 +1,7 @@
 import { Module } from "webpack"
 import oatStyle from "../../dist/styles/style_originalOat.module.css"
 import pixelStyle from "../../dist/styles/style_pixelArt.module.css"
+import highResStyle from "../../dist/styles/style_highRes.module.css"
 
 class KeyValueWrapper {
    
@@ -58,7 +59,7 @@ class CustomStyle {
 export const styleList = {
     oat: new CustomStyle("oat", oatStyle, "backgroundColor", "red"),
     pixel: new CustomStyle("pixel", pixelStyle, ["backgroundImage", "backgroundPosition", "backgroundRepeat", "backgroundAttachment", "backgroundColor"], [`url("./assets/pixel/To Move.png")`, "center center", "no-repeat", "scroll", "rgba(255, 0, 154, 0.3)"]),// center center no-repeat scroll rgba(255, 0, 153, 0.3)`),
-    highRes: new CustomStyle("highRes", oatStyle, "backgroundColor", "red"),
+    highRes: new CustomStyle("highRes", highResStyle, "backgroundColor", "rgb(212, 108, 81)"),
 }
 
 export function getCustomStyle(style: CustomStyle): Module { return style.module } ;
